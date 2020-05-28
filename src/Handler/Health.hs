43269aa3b8217f3371f1d4609b793a282b624441
Mike Solomon <mike@meeshkan.com>
2020-05-28 07:51:33 +0000

    Adds dockerfile

--- src/Handler/Health.hs
+++ src/Handler/Health.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
 module Handler.Health where
 
 import Import
+import qualified Data.Text as T
 
-getHealthR :: Handler Html
-getHealthR = error "Not yet implemented: getHealthR"
+getHealthR :: Handler Value
+getHealthR = return $ object
+        [ "works" .= T.pack "well"
+        ]
