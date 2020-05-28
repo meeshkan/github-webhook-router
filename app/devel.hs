92cfa8284685f98ebc720d51dad591c38e880758
Mike Solomon <mike@meeshkan.com>
2020-05-28 06:55:52 +0300

    Initial commit

---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "github-webhook-router" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
---
+++ src/Handler/Health.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Health where
+
+getHealthR :: Handler Html
+getHealthR = defaultLayout [whamlet|I'm healthy!|]
